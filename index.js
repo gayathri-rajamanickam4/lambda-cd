fba636cc43f39f7bafc0c8e97feb54e0e2025d7a
Gayathri Rajamanickam <gayathri_R15@infosys.com>
2018-09-22 15:58:22 +0530

    changed-2

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is:' + currentTime.toString(),
+        body: 'The time in Los Angeles is: ' + currentTime.toString(),
     });
 };
